3875501df172ce96acb69e19960d206b0ca7ab1a
salim <salim@salim-NUC7i5BNH>
2018-05-18 23:46:18 +0200

    monitoring progress

--- src/org/petctviewer/orthanc/monitoring/JDBC_Monitoring.java
+++ src/org/petctviewer/orthanc/monitoring/JDBC_Monitoring.java
@@ -26,17 +26,28 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.prefs.Preferences;
 
-public class JDBCConnector {
+public class JDBC_Monitoring<accessionNumber> {
 
 	private Connection connection;
-	private ArrayList<String> newName = new ArrayList<String>();
-	private ArrayList<String> newId = new ArrayList<String>();
-	private ArrayList<String> oldFirstName = new ArrayList<String>();
-	private ArrayList<String> oldLastName = new ArrayList<String>();
+	
 	private Preferences jprefer = Preferences.userRoot().node("<unnamed>/anonPlugin");
+	
+	public static void main(String arg[]) {
+		
+		try {
+			JDBC_Monitoring monitoring=new JDBC_Monitoring();
+			monitoring.InsertPatient("a", "b", "c", "dd","e","f");
+			monitoring.InsertStudy("gd", "dfg", "dfg", "dg","dg", "dfg", "dfg", "dg", "gd");
+		} catch (SQLException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+		
 
-	public JDBCConnector() throws ClassNotFoundException, SQLException{
-		Class.forName("com.mysql.jdbc.Driver");
+	public JDBC_Monitoring() throws ClassNotFoundException, SQLException{
+		//Class.forName("com.mysql.jdbc.Driver");
 		if(jprefer.get("dbAdress", null) != null && jprefer.get("dbPort", null) != null && jprefer.get("dbName", null) != null &&
 				jprefer.get("dbUsername", null) != null && jprefer.get("dbPassword", null) != null){
 			connection = DriverManager.getConnection("jdbc:mysql://" + jprefer.get("dbAdress", null) + ":" 
@@ -48,24 +59,37 @@ public class JDBCConnector {
 		connection.close();
 	}
 
-	public void newValuesQuery(Date birthday, String codeCentre) throws SQLException{
-		this.newName.clear();
-		this.newId.clear();
-		Statement st = connection.createStatement();
-		String sql = ("SELECT nameAnon, idAnon, name, firstName, codeCentre, birthday "
-				+ "FROM CTP "
-				+ "WHERE birthday = " + "\"" + birthday + "\""
-				+ " AND codeCentre = " + "\"" + codeCentre + "\""
-				+ ";");
-		ResultSet rs = st.executeQuery(sql);
-		while(rs.next()) { 
-			if(rs.getString(1).length() > 0){
-				this.newName.add(rs.getString(1));
-				this.newId.add(rs.getString(2));
-				this.oldLastName.add(rs.getString(3));
-				this.oldFirstName.add(rs.getString(4));
-			}
+	public void InsertPatient(String patientLastName, String patientFirstName, String patientID, String orthancID, String DOB, String Sex) {
+		
+		try {
+			Statement st = connection.createStatement();
+			String sql = ("INSERT INTO `patients`(`Orthanc_Patient_ID`, `Last_Name`, `First_Name`, `Patient_ID`, `DOB`, `Sex`) "
+					+ "VALUES ('"+orthancID+"','"+patientLastName+"','"+patientFirstName+"','"+patientID+"','"+DOB+"','"+Sex+"')");
+			System.out.println(sql);
+			boolean rs = st.execute(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		
+	
+	}
+	
+	public void InsertStudy(String studyID, String studyInstanceUID, String Orthanc_Study_ID, String accessionNumber, String institutionName, String referringPhysicianName,
+			String studyDate, String studyDescription, String studyTime  ) {
+	
+		try {
+			Statement st = connection.createStatement();
+			String sql = ("INSERT INTO `studies`(`accessionNumber`, `institutionName`, `referringPhysicianName`, `studyDate`, `studyDescription`, `studyID`, `studyInstanceUID`, `studyTime`, `Orthanc_Study_ID`) "
+					+ "VALUES ('"+accessionNumber+"','"+institutionName+"','"+referringPhysicianName+"','"+studyDate+"','"+studyDescription+"','"+studyID+"','"+studyInstanceUID+"','"+studyTime+"','"+Orthanc_Study_ID+"')");
+			System.out.println(sql);
+			boolean rs = st.execute(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	
 	}
 
 	public boolean sendSizeAndNewUID(String newName, String size, String studyInstanceUID) throws SQLException{
@@ -91,19 +115,5 @@ public class JDBCConnector {
 		return response > 0;
 	}
 	
-	public ArrayList<String> getNewName(){
-		return this.newName;
-	}
 
-	public ArrayList<String> getNewId(){
-		return this.newId;
-	}
-	
-	public ArrayList<String> getOldFirstName(){
-		return this.oldFirstName;
-	}
-	
-	public ArrayList<String> getOldLastName(){
-		return this.oldLastName;
-	}
 }
--- src/org/petctviewer/orthanc/monitoring/Tag_Monitoring.java
+++ src/org/petctviewer/orthanc/monitoring/Tag_Monitoring.java
@@ -1,5 +1,6 @@
 package org.petctviewer.orthanc.monitoring;
 
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -36,6 +37,16 @@ public class Tag_Monitoring {
 			public void run() {
 				
 				monitoring.makeMonitor();
+				JDBC_Monitoring db=null;
+				try {
+					db=new JDBC_Monitoring();
+				} catch (ClassNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 				
 				if (level.equals("patient")) {
 					for (int i=0 ; i<monitoring.newPatientID.size(); i++) {
@@ -47,18 +58,28 @@ public class Tag_Monitoring {
 							e.printStackTrace();
 						}
 						
-						getMainPatientTag((JSONObject) patientJson.get("MainDicomTags"));
+						HashMap<String, String> hashmapTagPatient=getMainPatientTag((JSONObject) patientJson.get("MainDicomTags"));
+						
+						//On ecrit dans la BDD
+						//SK BOOLEAN A GERER SI IL Y A UNE BDD D ACTIVE
+						db.InsertPatient(hashmapTagPatient.get("lastName"), hashmapTagPatient.get("firstName") ,  hashmapTagPatient.get("patientID"), monitoring.newPatientID.get(i),hashmapTagPatient.get("birthdate"), hashmapTagPatient.get("sex") );
 					}
 					
 				}
 				else if (level.equals("study")) {
 					for (int i=0 ; i<monitoring.newStudyID.size(); i++) {
 						StringBuilder sb=parametre.makeGetConnectionAndStringBuilder("/studies/"+monitoring.newStudyID.get(i));
+						HashMap<String, String> studyTag=null;
 						try {
-							getMainStudyTag((JSONObject) parser.parse(sb.toString()));
+							studyTag=getMainStudyTag((JSONObject) parser.parse(sb.toString()));
 						} catch (ParseException e) {
 							e.printStackTrace();
 						}
+						//On ecrit dans la BDD
+						//SK BOOLEAN A GERER SI IL Y A UNE BDD D ACTIVE
+						db.InsertPatient(studyTag.get("lastName"), studyTag.get("firstName") ,  studyTag.get("patientID"), monitoring.newPatientID.get(i), studyTag.get("birthdate"), studyTag.get("sex") );
+						db.InsertStudy(studyTag.get("studyID"), studyTag.get("studyInstanceUID"), studyTag.get(monitoring.newStudyID.get(i)), studyTag.get("accessionNumber"), studyTag.get("institutionName"), studyTag.get("referringPhysicianName"), studyTag.get("studyDate"), studyTag.get("studyDescription"), studyTag.get("studyTime"));
+						
 					}
 				}
 				else if (level.equals("serie")) {
@@ -72,7 +93,7 @@ public class Tag_Monitoring {
 						textAreaConsole.append("Shared-Tags"+sbSharedTags+",");
 						foundTags.put("Shared_Tags", sbSharedTags.toString());
 						
-						//Envoyer la Hashmap dans une methode qui va faire l'insert dans la BDD
+						//SK RESTE A FAIRE POUR LE NIVEAU SERIES (CREATION TABLE ET INSCRIPTION)
 						//SK A FAIRE POUR PATIENT ET STUDY
 						System.out.println(foundTags.toString());
 						
@@ -95,7 +116,7 @@ public class Tag_Monitoring {
         timer.scheduleAtFixedRate(timerTask, 0, (90*1000));
 	}
 	
-	private void getMainPatientTag(JSONObject mainPatientTag) {
+	private HashMap<String, String> getMainPatientTag(JSONObject mainPatientTag) {
 		
 		String birthDate=(String) mainPatientTag.get("PatientBirthDate");
 		String patientID=(String) mainPatientTag.get("PatientID");
@@ -108,14 +129,23 @@ public class Tag_Monitoring {
 		textAreaConsole.append("Sex= "+ patientSex+ ",");
 		textAreaConsole.append("DOB= " +birthDate+ "\n");
 		
+		String[] name =patientName.split("^");
+		HashMap<String, String> hashmapTagPatient=new HashMap<String, String>();
+		hashmapTagPatient.put("birthdate", birthDate);
+		hashmapTagPatient.put("patientID", patientID);
+		hashmapTagPatient.put("lastName", name[0]);
+		hashmapTagPatient.put("firstName", name[1]);
+		hashmapTagPatient.put("sex", patientSex);
+		
+		return hashmapTagPatient;
 	}
 	
-	private void getMainStudyTag(JSONObject jsonStudy) {
+	private HashMap<String, String> getMainStudyTag(JSONObject jsonStudy) {
 		textAreaConsole.append("New Study"+ "\n");
 		JSONObject jsonMainStudyTag=(JSONObject) jsonStudy.get("MainDicomTags");
 		
 		//On recupere les info Patients
-		getMainPatientTag((JSONObject) jsonStudy.get("PatientMainDicomTags"));
+		HashMap<String, String> hashmapTagPatient=getMainPatientTag((JSONObject) jsonStudy.get("PatientMainDicomTags"));
 		
 		//Info Study
 		String accessionNumber=(String) jsonMainStudyTag.get("AccessionNumber");
@@ -135,6 +165,17 @@ public class Tag_Monitoring {
 		textAreaConsole.append("studyID= " + studyID+ ",");
 		textAreaConsole.append("studyInstanceUID= "+ studyInstanceUID+ ",");
 		textAreaConsole.append("studyTime= " +studyTime+ "\n");
+		
+		hashmapTagPatient.put("accessionNumber", accessionNumber);
+		hashmapTagPatient.put("institutionName", institutionName);
+		hashmapTagPatient.put("referringPhysicianName", referringPhysicianName);
+		hashmapTagPatient.put("studyDate", studyDate);
+		hashmapTagPatient.put("studyDescription", studyDescription);
+		hashmapTagPatient.put("studyID", studyID);
+		hashmapTagPatient.put("studyInstanceUID", studyInstanceUID);
+		hashmapTagPatient.put("studyTime", studyTime);
+		
+		return hashmapTagPatient;
 		
 	}
 	
