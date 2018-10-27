f1a20a46c0f74fcdaad14ffcc13e380464fffc4e
salim <salim@salim-NUC7i5BNH>
2018-10-27 15:07:19 +0200

    refactor

--- src/org/petctviewer/orthanc/anonymize/VueAnon.java
+++ src/org/petctviewer/orthanc/anonymize/VueAnon.java
@@ -48,7 +48,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.prefs.Preferences;
 
-import javax.swing.AbstractAction;
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
@@ -103,7 +102,7 @@ import org.petctviewer.orthanc.setup.ConnectionSetup;
 import org.petctviewer.orthanc.setup.Run_Orthanc;
 
 
-public class VueAnon extends JFrame implements PlugIn{
+public class VueAnon extends JFrame implements PlugIn, ActionListener{
 	private static final long serialVersionUID = 1L;
 	
 	protected JTabbedPane tabbedPane;
@@ -1225,7 +1224,7 @@ public class VueAnon extends JFrame implements PlugIn{
 		
 		anonBtn = new JButton("Anonymize");
 		anonBtn.setPreferredSize(new Dimension(120,27));
-		anonBtn.addActionListener(new AnonAction());
+		anonBtn.addActionListener(this);
 		
 		//Label to show the currently selected profile in the main panel
 		JLabel profileLabel = new JLabel();
@@ -2702,261 +2701,243 @@ public class VueAnon extends JFrame implements PlugIn{
 			}
 		}
 	}
-	
-	
 
-	// Anonymization query
-	private class AnonAction extends AbstractAction{
-		private static final long serialVersionUID = 1L;
-		int dialogResult = JOptionPane.YES_OPTION;
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			dialogResult = JOptionPane.YES_OPTION;
-			anonCount = 0;
-			SwingWorker<Void,Void> workerRemoveScAndSr = new SwingWorker<Void,Void>(){
-				@Override
-				protected Void doInBackground() {
-					try {
-						modeleAnonStudies.removeScAndSr();
-					} catch(FileNotFoundException e){
-						// Ignore
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-					return null;
-				}
-			};
-			SwingWorker<Void,Void> worker = new SwingWorker<Void,Void>(){
-
-				@Override
-				protected Void doInBackground() {
-					//////////// Filling the user preferences ////////////
-					if(anonProfiles.getSelectedItem().equals("Custom")){
-						for(int i = 0; i < 2; i++){
-							if(bodyCharList[i].isSelected()){
-								if(jprefer.getInt("bodyCharac", 0) != i){
-									jprefer.putInt("bodyCharac", i);
-								}
-							}	
-						}
-						for(int i = 0; i < 2; i++){
-							if(datesList[i].isSelected()){
-								if(jprefer.getInt("Dates", 0) != i){
-									jprefer.putInt("Dates", i);
-								}
-							}
-						}
-						for(int i = 0; i < 2; i++){
-							if(bdList[i].isSelected()){
-								if(jprefer.getInt("BD", 0) != i){
-									jprefer.putInt("BD", i);
-								}
-							}
-						}
-						for(int i = 0; i < 2; i++){
-							if(ptList[i].isSelected()){
-								if(jprefer.getInt("PT", 0) != i){
-									jprefer.putInt("PT", i);
-								}
-							}
-						}
-						for(int i = 0; i < 2; i++){
-							if(scList[i].isSelected()){
-								if(jprefer.getInt("SC", 0) != i){
-									jprefer.putInt("SC", i);
-								}
-							}
-						}
-						for(int i = 0; i < 2; i++){
-							if(descList[i].isSelected()){
-								if(jprefer.getInt("DESC", 0) != i){
-									jprefer.putInt("DESC", i);
-								}
-							}
-						}
-					}
-
-					jprefer.put("profileAnon", anonProfiles.getSelectedItem().toString());
-
-					//Disable the anons button during anonymization
-					enableAnonButton(false);
-
-					anonBtn.setText("Anonymizing");
-					// SETTING UP THE CHOICES
+	public void actionPerformed(ActionEvent arg0) {
+		anonCount = 0;
+		
+		SwingWorker<Void,Void> worker = new SwingWorker<Void,Void>(){
+			int dialogResult=JOptionPane.YES_OPTION;
+			@Override
+			protected Void doInBackground() {
+				//////////// Filling the user preferences ////////////
+				if(anonProfiles.getSelectedItem().equals("Custom")){
 					for(int i = 0; i < 2; i++){
-						if(i == 0){
-							if(bodyCharList[i].isSelected())
-								bodyCharChoice = Choice.KEEP;
-							if(datesList[i].isSelected())
-								datesChoice = Choice.KEEP;
-							if(bdList[i].isSelected())
-								bdChoice = Choice.KEEP;
-							if(ptList[i].isSelected())
-								ptChoice = Choice.KEEP;
-							if(scList[i].isSelected())
-								scChoice = Choice.KEEP;
-							if(descList[i].isSelected())
-								descChoice = Choice.KEEP;
-						}else{
-							if(bodyCharList[i].isSelected())
-								bodyCharChoice = Choice.CLEAR;
-							if(datesList[i].isSelected())
-								datesChoice = Choice.CLEAR;
-							if(bdList[i].isSelected())
-								bdChoice = Choice.REPLACE;
-							if(ptList[i].isSelected())
-								ptChoice = Choice.CLEAR;
-							if(scList[i].isSelected())
-								scChoice = Choice.CLEAR;
-							if(descList[i].isSelected())
-								descChoice = Choice.CLEAR;					
+						if(bodyCharList[i].isSelected()){
+							if(jprefer.getInt("bodyCharac", 0) != i){
+								jprefer.putInt("bodyCharac", i);
+							}
+						}	
+					}
+					for(int i = 0; i < 2; i++){
+						if(datesList[i].isSelected()){
+							if(jprefer.getInt("Dates", 0) != i){
+								jprefer.putInt("Dates", i);
+							}
 						}
 					}
+					for(int i = 0; i < 2; i++){
+						if(bdList[i].isSelected()){
+							if(jprefer.getInt("BD", 0) != i){
+								jprefer.putInt("BD", i);
+							}
+						}
+					}
+					for(int i = 0; i < 2; i++){
+						if(ptList[i].isSelected()){
+							if(jprefer.getInt("PT", 0) != i){
+								jprefer.putInt("PT", i);
+							}
+						}
+					}
+					for(int i = 0; i < 2; i++){
+						if(scList[i].isSelected()){
+							if(jprefer.getInt("SC", 0) != i){
+								jprefer.putInt("SC", i);
+							}
+						}
+					}
+					for(int i = 0; i < 2; i++){
+						if(descList[i].isSelected()){
+							if(jprefer.getInt("DESC", 0) != i){
+								jprefer.putInt("DESC", i);
+							}
+						}
+					}
+				}
 
-					int i = 0;
-					int j = 0;
-					try {
-						if(anonProfiles.getSelectedItem().equals("Full clearing")){
-							if(modeleAnonStudies.getModalities().contains("NM") || 
-									modeleAnonStudies.getModalities().contains("PT")){
-								dialogResult = JOptionPane.showConfirmDialog (gui, 
-										"Full clearing is not recommended for NM or PT modalities."
-												+ "Are you sure you want to anonymize ?",
-												"Warning anonymizing PT/NM",
-												JOptionPane.WARNING_MESSAGE,
-												JOptionPane.YES_NO_OPTION);
-							}
-						}
-						if(modeleAnonStudies.getModalities().contains("US")){
-							JOptionPane.showMessageDialog (gui, 
-									"DICOM files with the US modality may have hard printed informations, "
-											+ "you may want to check your files.",
-											"Warning anonymizing US",
-											JOptionPane.WARNING_MESSAGE);
-						}
-						
-						// Checking if several anonymized patients have the same ID or not
-						boolean similarIDs = false;
-						ArrayList<String> newIDs = new ArrayList<String>();
-						for(int n = 0; n < anonPatientTable.getRowCount(); n++){
-							String newID = modeleAnonPatients.getPatient(anonPatientTable.convertRowIndexToModel(n)).getNewID();
-							if(newID != "" && !newIDs.contains(newID)){
-								newIDs.add(newID);
-							}else if(newIDs.contains(newID)){
-								similarIDs = true;
-							}
-						}
-						if(similarIDs){
+				jprefer.put("profileAnon", anonProfiles.getSelectedItem().toString());
+
+				//Disable the anons button during anonymization
+				enableAnonButton(false);
+
+				anonBtn.setText("Anonymizing");
+				// SETTING UP THE CHOICES
+				for(int i = 0; i < 2; i++){
+					if(i == 0){
+						if(bodyCharList[i].isSelected())
+							bodyCharChoice = Choice.KEEP;
+						if(datesList[i].isSelected())
+							datesChoice = Choice.KEEP;
+						if(bdList[i].isSelected())
+							bdChoice = Choice.KEEP;
+						if(ptList[i].isSelected())
+							ptChoice = Choice.KEEP;
+						if(scList[i].isSelected())
+							scChoice = Choice.KEEP;
+						if(descList[i].isSelected())
+							descChoice = Choice.KEEP;
+					}else{
+						if(bodyCharList[i].isSelected())
+							bodyCharChoice = Choice.CLEAR;
+						if(datesList[i].isSelected())
+							datesChoice = Choice.CLEAR;
+						if(bdList[i].isSelected())
+							bdChoice = Choice.REPLACE;
+						if(ptList[i].isSelected())
+							ptChoice = Choice.CLEAR;
+						if(scList[i].isSelected())
+							scChoice = Choice.CLEAR;
+						if(descList[i].isSelected())
+							descChoice = Choice.CLEAR;					
+					}
+				}
+
+				int i = 0;
+				int j = 0;
+				try {
+					
+					if(anonProfiles.getSelectedItem().equals("Full clearing")){
+						if(modeleAnonStudies.getModalities().contains("NM") || 
+								modeleAnonStudies.getModalities().contains("PT")){
 							dialogResult = JOptionPane.showConfirmDialog (gui, 
-									"You have defined 2 or more identical IDs for anonymized patients, which is not recommended."
-											+ " Are you sure you want to anonymize ?",
-											"Warning similar IDs",
+									"Full clearing is not recommended for NM or PT modalities."
+											+ "Are you sure you want to anonymize ?",
+											"Warning anonymizing PT/NM",
 											JOptionPane.WARNING_MESSAGE,
 											JOptionPane.YES_NO_OPTION);
 						}
-						
-						if(dialogResult == JOptionPane.YES_OPTION){
-
-							String substituteName = "A-" + jprefer.get("centerCode", "12345");
-
-							SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmm");
-							String substituteID = "A-" + df.format(new Date());
-
-							for(String patientID : modeleAnonStudies.getPatientIDs()){
-								String newName = modeleAnonPatients.getPatient(anonPatientTable.convertRowIndexToModel(j)).getNewName();
-								String newID = modeleAnonPatients.getPatient(anonPatientTable.convertRowIndexToModel(j)).getNewID();
-								String newStudyID = "";
-								if((newName == null || newName.equals("")) || (newID == null || newID.equals(""))){
-									anonCount++;
-								}
-								if(newName == null || newName.equals("")){
-									newName = substituteName + "^" + anonCount;
-									modeleAnonPatients.setValueAt(newName, anonPatientTable.convertRowIndexToModel(j), 3);
-								}
-
-								if(newID == null || newID.equals("")){
-									newID = substituteID + "^" + anonCount;
-									modeleAnonPatients.setValueAt(newID, anonPatientTable.convertRowIndexToModel(j), 4);
-								}
-
-								for(String uid : modeleAnonStudies.getOldOrthancUIDsWithID(patientID)){
-									String newDesc = modeleAnonStudies.getNewDesc(uid);
-									QueryAnon quAnon;
-									quAnon = new QueryAnon(connexionHttp, bodyCharChoice, datesChoice, bdChoice, ptChoice, scChoice, descChoice, newName, newID, newDesc);
-									quAnon.setQuery();
-									state.setText("<html>Anonymization state - " + (i+1) + "/" + modeleAnonStudies.getStudies().size() + 
-											" <font color='red'> <br>(Do not use the toolbox while the current operation is not done)</font></html>");
-									quAnon.sendQuery("studies", uid);
-									modeleAnonStudies.addNewUid(quAnon.getNewUID());
-									i++;
-									newStudyID = quAnon.getNewUID();
-									//Add anonymized study in export list
-									modeleExportStudies.addStudy(newName, newID, newStudyID);
-								}
-	
-								j++;
-							}
-							//Empty list
-							modeleAnonStudies.empty();
-							modeleAnonPatients.clear();
-							
-							
+					}
+					if(modeleAnonStudies.getModalities().contains("US")){
+						JOptionPane.showMessageDialog (gui, 
+								"DICOM files with the US modality may have hard printed informations, "
+										+ "you may want to check your files.",
+										"Warning anonymizing US",
+										JOptionPane.WARNING_MESSAGE);
+					}
+					
+					// Checking if several anonymized patients have the same ID or not
+					boolean similarIDs = false;
+					ArrayList<String> newIDs = new ArrayList<String>();
+					for(int n = 0; n < anonPatientTable.getRowCount(); n++){
+						String newID = modeleAnonPatients.getPatient(anonPatientTable.convertRowIndexToModel(n)).getNewID();
+						if(newID != "" && !newIDs.contains(newID)){
+							newIDs.add(newID);
+						}else if(newIDs.contains(newID)){
+							similarIDs = true;
 						}
-					} catch (IOException e1) {
-						e1.printStackTrace();
-					} catch (ParseException e) {
-						e.printStackTrace();
 					}
-					return null;
-				}
-
-				@Override
-				protected void done(){
-					if(scList[1].isSelected()){
-						workerRemoveScAndSr.execute();
+					if(similarIDs){
+						dialogResult = JOptionPane.showConfirmDialog (gui, 
+								"You have defined 2 or more identical IDs for anonymized patients, which is not recommended."
+										+ " Are you sure you want to anonymize ?",
+										"Warning similar IDs",
+										JOptionPane.WARNING_MESSAGE,
+										JOptionPane.YES_NO_OPTION);
 					}
-					//Re-enable anon button
-					enableAnonButton(true);
-					anonBtn.setText("Anonymize");
+					
 					if(dialogResult == JOptionPane.YES_OPTION){
-						state.setText("<html><font color='green'>The data has successfully been anonymized.</font></html>");
-						openCloseAnonTool(false);
-						pack();
-						tabbedPane.setSelectedIndex(1);
-						modeleAnonPatients.clear();
+
+						String substituteName = "A-" + jprefer.get("centerCode", "12345");
+
+						SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmm");
+						String substituteID = "A-" + df.format(new Date());
+
+						for(String patientID : modeleAnonStudies.getPatientIDs()){
+							String newName = modeleAnonPatients.getPatient(anonPatientTable.convertRowIndexToModel(j)).getNewName();
+							String newID = modeleAnonPatients.getPatient(anonPatientTable.convertRowIndexToModel(j)).getNewID();
+							String newStudyID = "";
+							if((newName == null || newName.equals("")) || (newID == null || newID.equals(""))){
+								anonCount++;
+							}
+							if(newName == null || newName.equals("")){
+								newName = substituteName + "^" + anonCount;
+								modeleAnonPatients.setValueAt(newName, anonPatientTable.convertRowIndexToModel(j), 3);
+							}
+
+							if(newID == null || newID.equals("")){
+								newID = substituteID + "^" + anonCount;
+								modeleAnonPatients.setValueAt(newID, anonPatientTable.convertRowIndexToModel(j), 4);
+							}
+
+							for(String uid : modeleAnonStudies.getOldOrthancUIDsWithID(patientID)){
+								String newDesc = modeleAnonStudies.getNewDesc(uid);
+								QueryAnon quAnon;
+								quAnon = new QueryAnon(connexionHttp, bodyCharChoice, datesChoice, bdChoice, ptChoice, scChoice, descChoice, newName, newID, newDesc);
+								quAnon.setQuery();
+								state.setText("<html>Anonymization state - " + (i+1) + "/" + modeleAnonStudies.getStudies().size() + 
+										" <font color='red'> <br>(Do not use the toolbox while the current operation is not done)</font></html>");
+								quAnon.sendQuery("studies", uid);
+								modeleAnonStudies.addNewUid(quAnon.getNewUID());
+								i++;
+								newStudyID = quAnon.getNewUID();
+								//Add anonymized study in export list
+								modeleExportStudies.addStudy(newName, newID, newStudyID);
+							}
+
+							j++;
+						}
+						
+						if(scList[1].isSelected()){
+							modeleAnonStudies.removeScAndSr();
+						}
+						
+						//Empty list
 						modeleAnonStudies.empty();
+						modeleAnonPatients.clear();
+						
 						
 					}
-					if(tableauExportStudies.getRowCount() > 0){
-						tableauExportStudies.setRowSelectionInterval(tableauExportStudies.getRowCount() - 1, tableauExportStudies.getRowCount() - 1);
-					}
-					modeleExportSeries.clear();
-					try {
-						if(modeleExportStudies.getRowCount() > 0){
-							String studyID = (String)tableauExportStudies.getValueAt(tableauExportStudies.getSelectedRow(), 5);
-							modeleExportSeries.addSerie(studyID);
-							tableauExportSeries.setRowSelectionInterval(0,0);
-						}
-					} catch (Exception e1) {
-						// IGNORE
-					}
-					//Si foncion a �t� fait avec le CTP on fait l'envoi auto A l'issue de l'anon
-					if(autoSendCTP) {
-						exportCTP.doClick();
-						autoSendCTP=false;
-					}
-					if(anonymizeListener!=null) {
-						anonymizeListener.AnonymizationDone();
-					}
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				} catch (ParseException e) {
+					e.printStackTrace();
 				}
-			};
-			if(!modeleAnonStudies.getOldOrthancUIDs().isEmpty()){
+				return null;
+			}
+
+			@Override
+			protected void done(){
+				
+				//Re-enable anon button
+				enableAnonButton(true);
+				anonBtn.setText("Anonymize");
 				if(dialogResult == JOptionPane.YES_OPTION){
-					worker.execute();
+					state.setText("<html><font color='green'>The data has successfully been anonymized.</font></html>");
+					openCloseAnonTool(false);
+					pack();
+					tabbedPane.setSelectedIndex(1);
+					modeleAnonPatients.clear();
+					modeleAnonStudies.empty();
+					
+				}
+				if(tableauExportStudies.getRowCount() > 0){
+					tableauExportStudies.setRowSelectionInterval(tableauExportStudies.getRowCount() - 1, tableauExportStudies.getRowCount() - 1);
+				}
+				modeleExportSeries.clear();
+				try {
+					if(modeleExportStudies.getRowCount() > 0){
+						String studyID = (String)tableauExportStudies.getValueAt(tableauExportStudies.getSelectedRow(), 5);
+						modeleExportSeries.addSerie(studyID);
+						tableauExportSeries.setRowSelectionInterval(0,0);
+					}
+				} catch (Exception e1) {
+					// IGNORE
+				}
+				//Si foncion a �t� fait avec le CTP on fait l'envoi auto A l'issue de l'anon
+				if(autoSendCTP) {
+					exportCTP.doClick();
+					autoSendCTP=false;
+				}
+				if(anonymizeListener!=null) {
+					anonymizeListener.AnonymizationDone();
 				}
 			}
+		};
+		if(!modeleAnonStudies.getOldOrthancUIDs().isEmpty()){
+				worker.execute();
 		}
 	}
+	
 	
 	public void enableAnonButton(boolean enable) {
 		anonBtn.setEnabled(enable);
@@ -2985,6 +2966,8 @@ public class VueAnon extends JFrame implements PlugIn{
 		anon.setLocationRelativeTo(null);
 		anon.setVisible(true);
 	}
+
+
 
 
 		
