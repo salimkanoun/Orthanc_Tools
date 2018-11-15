5cd89f8a4367ed95e77c745d391361558af097c6
salim <salim@salim-NUC7i5BNH>
2018-11-15 17:33:17 +0100

    clean

--- src/org/petctviewer/orthanc/ctpimport/CTP_Import_GUI.java
+++ src/org/petctviewer/orthanc/ctpimport/CTP_Import_GUI.java
@@ -52,7 +52,7 @@ public class CTP_Import_GUI extends VueAnon implements ImportListener, Anonymize
 		listePeers.setVisible(false);
 		listeAETExport.setVisible(false);
 		
-		addressFieldCTP.setText("http://localhost/");
+		addressFieldCTP.setText("http://kanoun.fr/");
 		
 		importCTP.addActionListener(new ActionListener() {
 
