45046a5fe85e2e277cfb6ac20b20d8be6c3d33bd
Salim Kanoun <salim.kanoun@gmail.com>
2019-05-03 11:54:30 +0200

    nothing

--- src/org/petctviewer/orthanc/OTP/standalone/Start_OTP_Import.java
+++ src/org/petctviewer/orthanc/OTP/standalone/Start_OTP_Import.java
@@ -5,6 +5,8 @@ import org.petctviewer.orthanc.anonymize.VueAnon;
 public class Start_OTP_Import   {
 
 	public static void main(String[] args) {
+		//System.setProperty("java.net.useSystemProxies", "true");
+		//VueAnon.jprefer=Preferences.systemNodeForPackage(Start_OTP_Import.class);
 		VueAnon anon=new VueAnon("OrthancCTP.json");
 		anon.setLocationRelativeTo(null);
 		anon.exportTabForOtp();
